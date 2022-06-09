427009a45f753fe63b693fd8d3528e13013c66a1
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-10 02:21:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+//Declare Global variables
+var customerName = 'bob'
+// let bestCustomer = "not bob";
+const leastFavoriteCustomer = "sarah"
+
+//Returns the customerName variable
+function customerName(){
+    return customerName;
+}
+console.log(upperCaseCustomerName1());
+// modifes the customerName variable
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName());
+
+//setBestCustomer
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+console.log(setBestCustomer());
+
+//Overwrites the best customer
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+console.log(overwriteBestCustomer());
+
+//Unsuccessfully tries to reassign the least favourite customer.
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "manuel";
+}
+console.log(leastFavoriteCustomer());
